850cc791fb7dc771e2791d2e6f52959f95a16690
chakkimu <murtuza.chakkiwala@ihg.com>
2019-08-06 19:39:19 +0530

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  barCodes = [];
+  selectedBarcode;
 
-}
+  constructor(
+    private barcodeScanner: BarcodeScanner
+  ) { }
+
+  onClick() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      this.selectedBarcode = barcodeData
+      alert(barcodeData)
+    }).catch(err => {
+      console.log('Error', err);
+      alert(err)
+    });
+  }
+} 
